0ae174672521cd3cf32f1e2b9e5e01d1910a723e
Juliusgeo <juliusgeo@gmail.com>
2018-06-19 18:25:27 -0400

    smoothed out issues
    added font size and family properties to popup
    changed name to directorspopup so it was less confusing
    added comments throughout

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 var portFromCS;
 
 function retrieve_title(obj){
+    //function to help map values from response to titles
+    //needed because for tv shows, the title is stored under original_name not title
     if(obj.title == undefined){
         return obj.original_name;
     }
@@ -11,21 +13,26 @@ function retrieve_title(obj){
 function connected(p) {
   portFromCS = p;
   console.log("connected to background script");
+  //add listener for messages from content script
   portFromCS.onMessage.addListener(function(m) {
-    console.log("selectedText: " +m.selectedText);
+    //sets up REST api details
     var key="c5a28ebdd06f8663b366b08d3221cbf2";
     var responsestring = "";
     var url="https://api.themoviedb.org/3/search/person?include_adult=false&page=1&query="+m.selectedText.toString().split(" ").join("%20")+"&language=en-US&api_key="+key;
+
+    //fetches search results from TMDb API
     fetch(url).then(function(response) {return response.json();}).then(function(data) {
+        //checks to make sure that there are search results for that query
         if(data.total_results == 0){
             responsestring = "Sorry, nothing found!";
-            console.log("nothing found for that query");
         }
+        //sets responsestring to concatenated string from array
         else{
+            //map is used to convert the array of json objects to array of title or original_name values
             titles = data.results[0].known_for.map(x=> retrieve_title(x));
             responsestring = titles.join(', ');
         }
-        console.log(responsestring);
+        //sends responsestring to content script so it can be displayed
         portFromCS.postMessage({response: responsestring});
     });
 
@@ -37,7 +44,7 @@ browser.runtime.onConnect.addListener(connected);
 
 
 browser.browserAction.onClicked.addListener(function (tab) {
-    // for the current tab, inject the "inject.js" file & execute it
+    //on page load, inject content scripts and style
     browser.runtime.onMessage.addListener(handleMessage);
     browser.tabs.executeScript({
         file: 'jquery-3.3.1.js',runAt: "document_start"
--- search.js
+++ search.js
@@ -23,44 +23,61 @@
         return t;
     };
 
-
+//initalize global variables
 var pageX;
 var pageY;
-
+var myPort;
 $(document).ready(function() {
+    //connect to background.js
     console.log("attemping to connect to port");
-    var myPort = browser.runtime.connect({name:"port-from-cs"});
+    myPort = browser.runtime.connect({name:"port-from-cs"});
     console.log("connected to port");
-    $("<ul class=\"tools\">Nothing found! Unfortunately</ul>").appendTo("body");
+
+    //initalize popup
+    $("<ul class=\"directorspopup\">Sorry, nothing found!</ul>").appendTo("body");
+
+    //add listener to handle response from background.js which will contain contents of popup
     myPort.onMessage.addListener(function(m) {
+            //hide popup if no result was obtained for selected text
             if(m.response == "Sorry, nothing found!"){
-                $('ul.tools').html("");
-                $('ul.tools').fadeOut(0);
+                $('ul.directorspopup').html("");
+                $('ul.directorspopup').fadeOut(0);
             }
+            //update directorspopup with response, and fade in the directorspopup
             else{
                 console.log(m.response);
-                $('ul.tools').html(m.response);
-                $('ul.tools').css({
+                $('ul.directorspopup').html(m.response);
+                $('ul.directorspopup').css({
                     'left': pageX + 5,
                     'top' : pageY - 55
                 }).fadeIn(200);
             }
-
     });
+
+    //function to handle sending the selected text to background.js
     function send_selectedtext(selt){
-        myPort.postMessage({selectedText: selt});
+        try {
+            myPort.postMessage({selectedText: selt});
+        }
+        catch{
+            //attempts to reconnect to port if connection fails
+            console.log("attemping to connect to port");
+            myPort = browser.runtime.connect({name:"port-from-cs"});
+            console.log("connected to port");
+        }
     }
     $(document).bind("mouseup", function() {
         send_selectedtext(x.Selector.getSelected().toString());
 
     });
     $(document).on("mousedown", function(e){
+        //on mousedown update the current coordinates to where the mouse is
         pageX = e.pageX;
         pageY = e.pageY;
+        //if nothing is selected, hide the directorspopup
         if(x.Selector.getSelected().toString() == ''){
-            $('ul.tools').fadeOut(200);
+            $('ul.directorspopup').fadeOut(200);
         }
-        console.log(pageX, pageY);
     });
 });
 })();
